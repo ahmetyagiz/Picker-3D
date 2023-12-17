98d866bef1760a542b6e9b63d6cefb45e4b64565
Ahmet Yağız <ahmetanteryagiz@gmail.com>
2023-12-17 12:09:27 +0300

    Level 1 Dropper & Progress Counter Fix

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -17,6 +17,7 @@ public class GameManager : MonoBehaviour
         if (fakeLevelIndex == 0)
         {
             PlayerPrefs.SetInt("FakeLevelIndex", 1);
+            fakeLevelIndex = 1;
         }
     }
     public void SceneRestart()

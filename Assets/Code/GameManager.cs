cf68cfec304d281979568d930e24c02dcf4f34b9
Ahmet Yağız <ahmetanteryagiz@gmail.com>
2023-12-17 13:12:17 +0300

    Random Levels When Reach the End

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -8,11 +8,15 @@ public class GameManager : MonoBehaviour
     public int fakeLevelIndex;
     private LevelSpawner levelSpawner;
 
+    public int tempInt;
+    public bool randomLevels;
+
     private void Awake()
     {
         levelSpawner = FindObjectOfType<LevelSpawner>();
         levelIndex = PlayerPrefs.GetInt("SceneIndex");
         fakeLevelIndex = PlayerPrefs.GetInt("FakeLevelIndex");
+        randomLevels = PlayerPrefs.GetInt("RandomLevels") == 1;
 
         if (fakeLevelIndex == 0)
         {
@@ -25,6 +29,7 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+
     public void NextScene()
     {
         levelIndex++;
@@ -32,9 +37,22 @@ public class GameManager : MonoBehaviour
 
         if (levelIndex == levelSpawner.levelData.levelPrefabs.Length)
         {
-            Debug.Log("Last level, returning to first level...");
-            levelIndex = 0;
+            randomLevels = true;
+            PlayerPrefs.SetInt("RandomLevels", randomLevels ? 1 : 0);
         }
+
+        if (randomLevels)
+        {
+            tempInt = Random.Range(0, levelSpawner.levelData.levelPrefabs.Length);
+
+            while (levelIndex == tempInt)
+            {
+                tempInt = Random.Range(0, levelSpawner.levelData.levelPrefabs.Length);
+            }
+            levelIndex = tempInt;
+        }
+
+        Debug.Log("Selected scene index: " + levelIndex);
 
         PlayerPrefs.SetInt("FakeLevelIndex", fakeLevelIndex);
         PlayerPrefs.SetInt("SceneIndex", levelIndex);
